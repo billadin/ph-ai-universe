e1cd01808a26955a7f8c453ca27532940a09e2b8
Mir Billadin <mir.billadin4@gmail.com>
2023-08-29 00:19:33 +0600

    card html added

---
+++ app.js
@@ -0,0 +1,8 @@
+const loadAI = async () => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+  const data = await res.json();
+  const tools = data.data;
+  console.log(tools);
+};
+
+loadAI();
