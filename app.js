ac6fc9383d8a23d5284e4a96e49ce756c04ad817
Mir Billadin <mir.billadin4@gmail.com>
2023-08-31 00:21:55 +0600

    added sorting by date functionality

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const cardContainer = document.getElementById('card-container');
 const seeMoreBtn = document.getElementById('see-more-btn');
 const seeLessBtn = document.getElementById('see-less-btn');
 const modalELement = document.getElementById("modal");
+const sortDate = document.getElementById('sort-date');
 
+
+let defaultTools;
 let allTools;
-
 // Load AI
 const loadAI = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
@@ -19,18 +21,18 @@ const loadAI = async () => {
 const displayTools = (tools) => {
     if (tools.length > 6) {
         seeMoreBtn.classList.remove('hidden');
-    }else {
-        seeMoreBtn.classList.add('hidden');
     }
     tools = tools.slice(0, 6);
+    defaultTools = tools;
     tools.forEach(tool => {
         createCard(tool);
-    });  
+    });
 }
 
  
 //Display all cards
 seeMoreBtn.addEventListener('click', () => {
+    defaultTools = 0;
     cardContainer.innerHTML = '';
     allTools.forEach(tool => {
         createCard(tool);
@@ -42,6 +44,7 @@ seeMoreBtn.addEventListener('click', () => {
 
 // Display less cards
 seeLessBtn.addEventListener('click', () => {
+    allTools = 0;
     cardContainer.innerHTML = '';
     loadAI();
     seeLessBtn.classList.add('hidden');
@@ -61,16 +64,16 @@ const createCard = (tool) => {
             <h2 class="card-title mb-4">Features</h2>
             <div class="mb-6">
                 <ol class="list-decimal list-inside text-start">
-                    <li>${features[0]}</li>
-                    <li>${features[1]}</li>
-                    <li>${features[2]}</li>
+                    <li>${features[0] ? features[0] : ' No data found'}</li>
+                    <li>${features[1] ? features[1] : ' No data found'}</li>
+                    <li>${features[2] ? features[2] : ' No data found'}</li>
                 </ol>
             </div>
             <hr class="h-[1px] mb-6 bg-slate-600">
             <div class="flex justify-between items-center">
                 <div>
                     <h2 class="card-title mb-4">${name}</h2>
-                    <P>${published_in}</P>
+                    <P class="publish-date" id="${id}">${published_in}</P>
                 </div>
                 <div>
                     <button class="btn btn-sm details-btn" id="${id}">Details</button>
@@ -103,7 +106,7 @@ const loadAIDetails = async (id) => {
 const addModalDetails = (toolsDetails) => {
     console.log(toolsDetails);
     const {description, features, pricing, integrations, image_link, accuracy, input_output_examples} = toolsDetails;
-    modalELement.innerHTML = `<form method="dialog" class="modal-box max-w-[70%] max-h-none w-[70&] p-32 mx-40">
+    modalELement.innerHTML = `<form method="dialog" class="modal-box max-w-[70%] w-[70&] p-32 mx-40">
     <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     <div class="details-container grid grid-cols-1 md:grid-cols-2 gap-5">
         <div class="bg-[#eb57570d] rounded-2xl p-7 border border-[#EB5757]">
@@ -111,7 +114,7 @@ const addModalDetails = (toolsDetails) => {
             <div class="pricing grid grid-cols-3 gap-5 my-6">
                     <div class="text-base font-bold px-6 py-6 rounded-2xl bg-white text-[#03A30A] text-center">${pricing ? pricing[0]?.price : 'No data found'}<br>Basic</div>
                     <div class="text-base font-bold px-6 py-6 rounded-2xl bg-white text-[#F28927] text-center">${pricing ? pricing[1]?.price : 'No data found'}<br>Pro</div>
-                    <div class="text-base font-bold px-6 py-6 rounded-2xl bg-white text-[#EB5757] text-center">Contact<br>us<br>Enterprise</div>
+                    <div class="text-base font-bold px-6 py-6 rounded-2xl bg-white text-[#EB5757] text-center">${pricing ? pricing[2]?.price : 'No data found'}<br>Enterprise</div>
             </div>
             <div class="feature grid grid-cols-2 gap-6 mb-14">
                 <div class="features">
@@ -125,9 +128,9 @@ const addModalDetails = (toolsDetails) => {
                 <div class="integrations">
                     <h3 class="text-2xl font-semibold mb-4">Integrations</h3>
                     <ul class="list-disc list-inside text-[#585858]">
-                        <li>${toolsDetails.integrations ? toolsDetails.integrations[0] : 'No data found'}</li>
-                        <li>${toolsDetails.integrations ? toolsDetails.integrations[1] : 'No data found'}</li>
-                        <li>${toolsDetails.integrations ? toolsDetails.integrations[2] : 'No data found'}</li>
+                        <li>${integrations ? (integrations[0] ? integrations[0] : 'No data found') : 'No data found'}</li>
+                        <li>${integrations ? (integrations[1] ? integrations[1] : 'No data found') : 'No data found'}</li>
+                        <li>${integrations ? (integrations[2] ? integrations[2] : 'No data found') : 'No data found'}</li>
                     </ul>
                 </div>
             </div>
@@ -145,5 +148,29 @@ const addModalDetails = (toolsDetails) => {
 </form>`;
 };
 
+
+sortDate.addEventListener("click", () => {
+  const sortTools = (currentTools) => {
+    console.log(currentTools);
+    currentTools.forEach((tool) => {
+      const toolDate = tool.published_in;
+      const date = new Date(toolDate);
+      tool.date = date;
+    });
+    currentTools.sort((a, b) => {
+      return a.date - b.date;
+    });
+    cardContainer.innerHTML = "";
+    currentTools.forEach((tool) => {
+      createCard(tool);
+    });
+  };
+  
+  if (defaultTools != 0) {
+    sortTools(defaultTools);
+  } else if (allTools != 0) {
+    sortTools(allTools);
+  }
+});
 
 loadAI();
